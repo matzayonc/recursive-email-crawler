a128fa5df63daeae0bb8ba899dbd22a59b220325
matzayonc <matzayonc@gmail.com>
2020-09-12 16:43:55 +0200

    no errors, no work

--- server.js
+++ server.js
@@ -20,42 +20,71 @@ async function search(site, level = levels, res = null) {
 
 	if (!/http[s]?:\/\//.test(site)) site = "http://" + site
 
-	let emails = []
-	let response = await fetch(site)
-	let html = await response.text()
-	//console.log(html)
 	let domain = site.match(/http[s]?:\/\/[^\/]*/)[0]
+	console.log("domain:", domain, "for:", site)
+
+	let response,
+		html,
+		emails = []
+
+	try {
+		emails = []
+		response = await fetch(site)
+		html = await response.text()
+	} catch (error) {
+		console.error("Error while fetching:", site)
+		return []
+	}
+
+	//console.log(html)
 
 	emails = emails.concat(findEmails(html))
 	let otherSites = findLinks(html)
 	otherSites = otherSites.map((i) => {
 		if (i[0] == "/") return domain + i
-		else return i
+		else if (/http[s]?:\/\//.test(i)) return i
+		else return domain + "/" + i
 	})
 
 	console.log("links", otherSites)
 
-	if (level - 1)
-		for (let i = 0; i < otherSites.length && i < 10; i++) {
-			let newEmails = await search(otherSites[i], level - 1)
-			if (newEmails) emails = emails.concat(newEmails)
-		}
+	if (level - 1) {
+		let promises = []
+		for (let i = 0; i < otherSites.length && i < 10; i++)
+			promises.push(search(otherSites[i], level - 1))
 
-	if (res) {
-		emails = uniq(emails)
-		console.log("emails", emails)
-		res.write("emails: " + emails)
-		res.end()
+		Promise.all(promises).then((values) => {
+			console.log(values)
+
+			for (let i of values) {
+				console.log(i)
+				emails = emails.concat(i)
+				console.log(emails)
+			}
+
+			if (res) respondWithEmails(emails, res)
+			return emails
+		})
+	} else {
+		respondWithEmails(emails, res)
+		return emails
 	}
+}
 
-	return emails
+function respondWithEmails(emails, res = null) {
+	if (!res) return
+
+	emails = uniq(emails)
+	console.log("emails", emails)
+	res.write("emails: " + emails)
+	res.end()
 }
 
 function findEmails(str) {
 	return [...str.matchAll(/\w+\@\w+\.\w+/g)].map((i) => i[0])
 }
 function findLinks(str) {
-	let links = [...str.matchAll(/<a\shref=\"[^#"][^\"]+/g)]
+	let links = [...str.matchAll(/<a\shref=\"[^#"\.][^\"]+/g)]
 	//console.log(links)
 	return links.map((i) => (i = i[0].substr(9)))
 }
