ae04e31bcef54ce36a70d41b621f30516b390ebf
matzayonc <matzayonc@gmail.com>
2020-09-12 15:48:04 +0200

    working

--- server.js
+++ server.js
@@ -6,46 +6,65 @@ const fetch = require("node-fetch")
 const hostname = "127.0.0.1"
 const port = 3000
 
-async function search(site, res = null) {
+const searchLevel = 3
+
+function uniq(a) {
+	return a.sort().filter(function (item, pos, ary) {
+		return !pos || item != ary[pos - 1]
+	})
+}
+
+async function search(site, level = levels, res = null) {
+	if (!site) return []
+	console.log("start search in", site)
+
+	if (!/http[s]?:\/\//.test(site)) site = "http://" + site
+
 	let emails = []
-	let response = await fetch("http://" + site)
+	let response = await fetch(site)
 	let html = await response.text()
-	console.log(html)
+	//console.log(html)
+	let domain = site.match(/http[s]?:\/\/[^\/]*/)[0]
 
 	emails = emails.concat(findEmails(html))
+	let otherSites = findLinks(html)
+	otherSites = otherSites.map((i) => {
+		if (i[0] == "/") return domain + i
+		else return i
+	})
 
-	/*
-	fetch("http://" + site)
-		.then((res) => res.text())
-		.then((body) =>
-			console.log(
-				body +
-					"\n\nemails: " +
-					findEmails(body) +
-					"\n\nlinks: " +
-					findLinks(body)
-			)
-		)*/
+	console.log("links", otherSites)
+
+	if (level - 1)
+		for (let i = 0; i < otherSites.length && i < 10; i++) {
+			let newEmails = await search(otherSites[i], level - 1)
+			if (newEmails) emails = emails.concat(newEmails)
+		}
+
 	if (res) {
+		emails = uniq(emails)
+		console.log("emails", emails)
 		res.write("emails: " + emails)
 		res.end()
 	}
+
 	return emails
 }
 
 function findEmails(str) {
-	return str.match(/\w+\@\w+\.\w+/)
+	return [...str.matchAll(/\w+\@\w+\.\w+/g)].map((i) => i[0])
 }
 function findLinks(str) {
-	let links = str.match(/<a\shref=\"[^\"]+/)
-	return links.map((i) => (i = i.substr(9)))
+	let links = [...str.matchAll(/<a\shref=\"[^#"][^\"]+/g)]
+	//console.log(links)
+	return links.map((i) => (i = i[0].substr(9)))
 }
 
 function findWebsites(str) {}
 
 const server = http.createServer((req, res) => {
 	var q = url.parse(req.url, true)
-	console.log(q.pathname)
+	//console.log(q.pathname)
 
 	switch (url.parse(req.url, true).pathname) {
 		case "/":
@@ -60,13 +79,7 @@ const server = http.createServer((req, res) => {
 		case "/search":
 			let site = q.query.website
 			res.writeHead(200, { "Content-Type": "text/plain" })
-			search(site, res)
-
-		/*
-		default:
-			res.writeHead(200, { "Content-Type": "text/plain" })
-			res.write("no such path")
-			res.end()*/
+			search(site, searchLevel, res)
 	}
 })
 
