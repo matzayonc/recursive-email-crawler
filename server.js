b0b51f8887a5457f9a777760bf29e88163ddf130
matzayonc <matzayonc@gmail.com>
2020-09-12 13:42:59 +0200

    created search function

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http")
+const fs = require("fs")
+var url = require("url")
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = "127.0.0.1"
+const port = 3000
+
+function search(req) {
+	let site = req.query.website
+	return "erw"
+}
 
 const server = http.createServer((req, res) => {
+	var q = url.parse(req.url, true)
+	console.log(q.pathname)
 
-    fs.readFile('./index.html', function(err, data) {
-        if(err)
-            console.error(err)
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data)
-        res.end()
-    })
+	switch (url.parse(req.url, true).pathname) {
+		case "/":
+			fs.readFile("./index.html", function (err, data) {
+				if (err) console.error(err)
+				res.writeHead(200, { "Content-Type": "text/html" })
+				res.write(data)
+				res.end()
+			})
+			break
+
+		case "/search":
+			res.writeHead(200, { "Content-Type": "text/javascript" })
+			res.write(search(url.parse(req.url, true)))
+			res.end()
+
+		/*
+		default:
+			res.writeHead(200, { "Content-Type": "text/plain" })
+			res.write("no such path")
+			res.end()*/
+	}
 })
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+	console.log(`Server running at http://${hostname}:${port}/`)
+})
